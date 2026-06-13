1eae0a12f2f494cf35e97dd23fd3e3029da6969b
Zolric <64628722+Zolric@users.noreply.github.com>
2026-06-13 10:27:36 +0200

    update info synchthing

--- syncthing/rootfs/etc/cont-init.d/run.sh
+++ syncthing/rootfs/etc/cont-init.d/run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
-bashio::log.info "Protection Mode is $(bashio::addon.protected)"
+bashio::log.info "Protection Mode is $(bashio::app.protected)"
 bashio::log.info "montage usb Disk Hass.io"
 disk_s=$(bashio::config 'disk_s')
 type_s=$(bashio::config 'type_s')
+version_syncthing=$(syncthing version)
 
 # Mount CIFS Share if configured and if Protection Mode is active
 #ls -ltr /disk1
@@ -17,4 +18,4 @@ else
 fi
 mount ${typeopt} /dev/disk/by-label/${disk_s} /disk1 
 
-
+bashio::log.info "Version Syncthing: ${version_syncthing}"
